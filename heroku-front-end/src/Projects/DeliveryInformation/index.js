5692f81d75540e5735465cafd4344a17092f0c8b
Ramon Bosi <ramombosi2015@gmail.com>
2022-07-29 09:08:07 -0300

    Creating a payment method form

--- heroku-front-end/src/Projects/DeliveryInformation/index.js
+++ heroku-front-end/src/Projects/DeliveryInformation/index.js
@@ -10,13 +10,14 @@ export default function DeliveryInformation(){
         <Routes>
             <Route path = '/' element = {<Login/>}/>
             <Route path = 'user/create' element = {<UserForm/>}/>
+            
             <Route path = 'user/:idUser/update' element = {<UserForm/>}/>
 
             <Route path = 'address/user/:idUser/create' element = {<AddressForm/>}/>
             <Route path = 'address/user/:idUser/update/:idAddress' element = {<AddressForm/>}/>
 
             <Route path = 'paymentMethods/user/:idUser/create' element = {<PaymentMethodsForm/>}/>
-            <Route path = 'paymentMethods/user/:idUser/update/:paymentMethods' element = {<PaymentMethodsForm/>}/>
+            <Route path = 'paymentMethods/user/:idUser/update/:idPaymentMethods' element = {<PaymentMethodsForm/>}/>
             
             <Route path = '*' element = {<h1 style={{color: 'rgba(255,255,255,1)'}}>Página não encontrada</h1>}/>
         </Routes>
