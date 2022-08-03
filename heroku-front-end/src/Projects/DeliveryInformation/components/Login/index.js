29653597cb6103ae91ea11f5ee3b5c32d80b761f
Ramon Bosi <ramombosi2015@gmail.com>
2022-08-03 11:53:31 -0300

    login system

--- heroku-front-end/src/Projects/DeliveryInformation/components/Login/index.js
+++ heroku-front-end/src/Projects/DeliveryInformation/components/Login/index.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useContext } from 'react'
 import Form from '../Form'
 import { loginValidation } from '../../validations'
 import { server } from '../../server'
+import { UserContext } from '../../contexts/userContext'
 
 export default function Login(){
 
@@ -11,13 +13,26 @@ export default function Login(){
         resolver: yupResolver(loginValidation)
     })
 
+    const { setIdUser, goToPage } = useContext(UserContext)
+
     const loginUser = (dataUser) => {
 
         const user = dataUser
 
         server.post('/usuario/login',{email: user.email, senha: user.password})
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err))
+        .then((res) => {
+            const data = res.data
+
+            if(data.error){
+                alert(data.response)
+            }else{
+                const id = data.response.idUsuario
+                setIdUser(id)
+                alert(data.response.message)
+                goToPage(`user/${id}/userData`)
+            }
+        })
+        .catch(() => console.log('Algo deu errado na requisição'))
     }
 
     return(
