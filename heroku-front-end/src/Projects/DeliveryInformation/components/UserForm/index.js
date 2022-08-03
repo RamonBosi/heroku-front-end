cab08980859345395de44da63c82728a8507fdc7
Ramon Bosi <ramombosi2015@gmail.com>
2022-08-03 19:12:52 -0300

    Update index.js

--- heroku-front-end/src/Projects/DeliveryInformation/components/UserForm/index.js
+++ heroku-front-end/src/Projects/DeliveryInformation/components/UserForm/index.js
@@ -1,39 +1,123 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { validationUser } from '../../validations';
+import { useContext, useEffect, useState } from 'react';
 import Form from "../Form";
+import { server } from '../../server';
+import { UserContext } from '../../contexts/userContext';
 
 export default function UserForm(){
+    
+    const { goToPage, setIdUser, getIdUser } = useContext(UserContext)
+    
+    const { idUser } = useParams()
+    
+    const [updateUser, setUpdateUser] = useState()
+    
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(validationUser)
     })
-    
-    const dataSubmit = (data) => console.log(data) 
+
+    useEffect(() =>{
+        if(idUser){
+
+            server.get(`/usuario/${getIdUser()}/dadosUsuario`)
+            .then((res) => {
+                const data = res.data
+
+                if(data.error){
+                    alert('Não foi possível recuperar os dados')
+                }else{
+
+                    const userData = {
+                        nome: data.response.nome,
+                        cpf: data.response.cpf,
+                        email: data.response.email,
+                        senha: data.response.senha
+                    }
+                    setUpdateUser(userData)
+                }
+            })
+            .catch(() => alert('Não foi possível recuperar os dados'))
+        }
+    },[])
+
+    const dataSubmit = async (data) => {
+
+        const user = {
+            nome: data.nome,
+            cpf:data.cpf,
+            email:data.email,
+            senha: data.password
+        }
+
+        if(idUser){
+            //Atualizar
+            console.log(`Usuario de id ${getIdUser()} atualizado`)
+        }else{
+            //Cadastrar
+            const createdUser = await server.post('/usuario/cadastrar',user)
+            .then((res) =>{
+                if(res.data.error){
+                    return res.data
+                }else{
+                    return res.data
+                }
+            })
+            .catch(() => {return {error: true, response: 'Não foi possível cadastrar o usuário, tente mais tarde'}})
+           
+            if(createdUser.error){
+                alert(createdUser.response)
+            }else{
+                server.post('/usuario/login',{email: user.email, senha: user.senha})
+                .then((res) => {
+                    const data = res.data
+
+                    if(data.error){
+                        alert(data.response)
+                    }else{
+                        const id = data.response.idUsuario
+                        setIdUser(id)
+                        alert('Usuário criado com sucesso!')
+                        goToPage(`user/${id}/userData`)
+                    }
+                })
+                .catch(() => {
+                    alert('Usuário criado, tente fazer login dentro de alguns minutos')
+                    goToPage(`/deliveryInformation`)
+                })
+            }
+        }
+    }
 
     return(
         <Form
             handleSubmit={handleSubmit(dataSubmit)}
-            formTitle={<h2>Cadastrar Usuário</h2>}
+            formTitle={<h2>{idUser ? 'Atualizar' : 'Cadastrar'} usuário</h2>}
             formInput={
                 <>
                     <input 
+                        defaultValue={updateUser?.nome}
                         type = 'text' 
                         placeholder="Nome" 
                         {...register('nome')}/>
                     <p>{errors.nome?.message}</p>
                     <input 
+                        defaultValue={updateUser?.cpf}
                         type = "number" 
                         placeholder="CPF" 
                         {...register('cpf')}/>
                     <p>{errors.cpf?.message}</p>
                     <input 
+                        defaultValue={updateUser?.email}
                         type = 'text' 
                         placeholder="Email" 
                         {...register('email')}/>
                     <p>{errors.email?.message}</p>
                     <input 
-                        type = 'password' 
+                        defaultValue={updateUser?.senha}
+                        type = {idUser ? 'text' : 'password'} 
                         placeholder="Senha" 
                         {...register('password')}/>
                     <p>{errors.password?.message}</p>
@@ -41,7 +125,9 @@ export default function UserForm(){
             }
             formBtn={
                 <>
-                    <button type = 'submit'>Cadastrar</button>
+                    <button type = 'submit'>
+                        {idUser ? 'Atualizar' : 'Cadastrar'} usuário
+                    </button>
                     <button>
                         <Link to = '/deliveryInformation'>Cancelar</Link>
                     </button>
