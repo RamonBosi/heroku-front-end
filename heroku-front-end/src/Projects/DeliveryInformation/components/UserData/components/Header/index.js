8a02345100376ba6683e7a86bccd877cdb941149
Ramon Bosi <ramombosi2015@gmail.com>
2022-08-05 10:56:33 -0300

    requests to update and delete user

--- heroku-front-end/src/Projects/DeliveryInformation/components/UserData/components/Header/index.js
+++ heroku-front-end/src/Projects/DeliveryInformation/components/UserData/components/Header/index.js
@@ -1,16 +1,55 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { MdLogout } from 'react-icons/md'
+import { UserContext } from '../../../../contexts/userContext'
+import { server } from '../../../../server'
 
 export default function Header(){
+
+    const { goToPage, getIdUser, removeIdUser } = useContext(UserContext)
+
+    const deleteUser = () =>{
+        server.delete(`/usuario/${getIdUser()}/deletar`)
+        .then((res)=>{
+            const data = res.data
+
+            if(data.error){
+                alert(data.response)
+            }else{
+                alert(data.response)
+                removeIdUser()
+                goToPage(`/deliveryInformation`)
+            }
+        })
+        .catch(() => alert('Não foi possível deletar, tente mais tarde'))
+    }
+
+    const logoutUser = () =>{
+        removeIdUser()
+        goToPage(`/deliveryInformation`)
+    }
+
     return(
         <header className='userData-header'>
             <div className='userData-container'>
                 <h1>Informações de entregas</h1>
                 <div>
                     <div className='userData-header-btn'>
-                        <button className = 'btn-update-user'>Atualizar conta</button>
-                        <button className = 'btn-delete-user'>Deletar conta</button>
+                        <button className = 'btn-update-user'>
+                            <Link to = {`/deliveryInformation/user/${getIdUser()}/update`}>
+                                Atualizar conta
+                            </Link>
+                        </button>
+                        <button 
+                            className = 'btn-delete-user' 
+                            onClick = {deleteUser}>
+                            Deletar conta
+                        </button>
                     </div>
-                    <div className='userData-header-logout' title='Sair'>
+                    <div 
+                        className='userData-header-logout' 
+                        title='Sair'
+                        onClick={logoutUser}>
                         <MdLogout/>
                     </div>
                 </div>
--- heroku-front-end/src/Projects/DeliveryInformation/contexts/userContext/index.js
+++ heroku-front-end/src/Projects/DeliveryInformation/contexts/userContext/index.js
@@ -8,7 +8,8 @@ export default function UserContextProvider({children}){
     const value = {
         goToPage: useNavigate(), 
         setIdUser(idUser){localStorage.setItem('idUser', idUser)},
-        getIdUser(){return localStorage.getItem('idUser')}
+        getIdUser(){return localStorage.getItem('idUser')},
+        removeIdUser(){localStorage.removeItem('idUser')}
     }
     
     return(
