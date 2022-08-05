b8e8406844915c9a19584fc2f63ef9d9236860a8
Ramon Bosi <ramombosi2015@gmail.com>
2022-08-05 11:43:01 -0300

    Request to get user data

--- heroku-front-end/src/Projects/DeliveryInformation/components/UserData/components/Main/index.js
+++ heroku-front-end/src/Projects/DeliveryInformation/components/UserData/components/Main/index.js
@@ -1,12 +1,35 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../../../contexts/userContext";
+import { server } from "../../../../server";
 import User from "./components/User";
 import UserAddress from "./components/UserAddress";
 import UserPaymentMethods from "./components/UserPaymentMethods";
 
 export default function Main(){
+
+    const { getIdUser } = useContext(UserContext)
+
+    const [userData, setUserData] = useState()
+
+    useEffect(() =>{
+        server.get(`usuario/${getIdUser()}/pegarDados`)
+        .then((res) => {
+            const data = res.data
+
+            if(data.error){
+                alert(data.response)
+            }else{
+                console.log(data.response)
+                setUserData(data.response)
+            }
+        })
+        .catch(() => alert('Não foi possível pegar os dados, tente mais tarde'))
+    },[])
+
     return(
         <main className = 'user-data'>
             <div className = 'user-data-container'>
-                <User userName={'Ramon'}/>
+                <User userName={userData?.usuario.nome}/>
                 <UserAddress/>
                 <UserPaymentMethods/>
             </div>
