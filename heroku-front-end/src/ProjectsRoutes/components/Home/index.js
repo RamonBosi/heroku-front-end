d45cfc58d28c7575906bd2c8262107df0c2cfb3e
Ramon Bosi <ramombosi2015@gmail.com>
2022-08-06 15:32:03 -0300

    Styling the Home

--- heroku-front-end/src/ProjectsRoutes/components/Home/index.js
+++ heroku-front-end/src/ProjectsRoutes/components/Home/index.js
@@ -1,20 +1,25 @@
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { RepositoryContext } from '../../RepositoryContext'
+import './styles.scss'
 
 export default function Home(){
 
     const { repoDeliveryInformation } = useContext(RepositoryContext)
 
     return(
-        <>
-        <h1>Front-end de projetos com servidor hospedado na Heroku</h1>
-            <h2>Projetos</h2>
-            <ul>
-                <li>
-                    <Link to = {`${repoDeliveryInformation}`}>Informações de entregas</Link>
-                </li>
-            </ul>
-        </>
+        <div className='home'>
+            <div className='home-container'>
+                <h1>
+                    Front-end de projetos com servidor hospedado na 
+                    <span className='heroku-logo'> Heroku</span>
+                </h1>
+                <ul>
+                    <li>
+                        <Link to = {`${repoDeliveryInformation}`}>Informações de entregas</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
