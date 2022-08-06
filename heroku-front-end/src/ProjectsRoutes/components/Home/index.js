7b1d4453e5f73402fd26f28dbb63e3ba24c002a7
Ramon Bosi <ramombosi2015@gmail.com>
2022-08-06 12:07:21 -0300

    Update index.js

--- heroku-front-end/src/ProjectsRoutes/components/Home/index.js
+++ heroku-front-end/src/ProjectsRoutes/components/Home/index.js
@@ -1,13 +1,18 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { RepositoryContext } from '../../RepositoryContext'
 
 export default function Home(){
+
+    const { repoDeliveryInformation } = useContext(RepositoryContext)
+
     return(
         <>
         <h1>Front-end de projetos com servidor hospedado na Heroku</h1>
             <h2>Projetos</h2>
             <ul>
                 <li>
-                    <Link to = '/deliveryInformation'>Informações de entregas</Link>
+                    <Link to = {`${repoDeliveryInformation}`}>Informações de entregas</Link>
                 </li>
             </ul>
         </>
